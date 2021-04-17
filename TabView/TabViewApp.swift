d0ead00dcc77383c053aa9309cb215aa31f27fcd
Std1o <37378410+Std1o@users.noreply.github.com>
2021-04-17 21:58:11 +0300

    Initial commit

---
+++ TabView/TabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewApp.swift
+//  TabView
+//
+//  Created by Илья on 17.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct TabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
