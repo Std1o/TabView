8219e0f1ad32e3875a08e3d1c3f85fdc4a13079e
Std1o <37378410+Std1o@users.noreply.github.com>
2021-04-17 22:06:15 +0300

    tabview

--- TabView/ContentView.swift
+++ TabView/ContentView.swift
@@ -8,9 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selection = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            TabView(selection: $selection) {
+                List(1...10, id: \.self) { index in
+                    NavigationLink(
+                        destination: Text("Item #\(index) Details"),
+                        label: {
+                            Text("Item #\(index)")
+                                .font(.system(size: 20, weight: .bold, design: .rounded))
+                        })
+                    
+                }
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
+                .tag(0)
+                
+                Text("Bookmark Tab")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .tabItem {
+                        Image(systemName: "bookmark.circle.fill")
+                        Text("Bookmark")
+                    }
+                    .tag(1)
+                
+                Text("Video Tab")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .tabItem {
+                        Image(systemName: "video.circle.fill")
+                        Text("Video")
+                    }
+                    .tag(2)
+                
+                Text("Profile Tab")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .tabItem {
+                        Image(systemName: "person.crop.circle")
+                        Text("Profile")
+                    }
+                    .tag(3)
+            }
+            .accentColor(.red)
+            .onAppear() {
+                UITabBar.appearance().barTintColor = .white
+            }
+            
+            .navigationTitle("TabView Demo")
+        }
     }
 }
 
